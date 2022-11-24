6cbeb95d6cdd955dd97e2353346d12da6e4c7c26
Watcharith <118547469+Watcharith@users.noreply.github.com>
2022-11-24 22:51:08 +0700

    tkinter_first_attemp

--- tkinter_test.py
+++ tkinter_test.py
@@ -6,12 +6,40 @@ from pandas.plotting import scatter_matrix
 import yfinance as yf
 import tkinter as tk
 
-name = input().upper()
-started = input()
-ended = input()
-sel = input().capitalize()
+tkin = tk.Tk()
+tkin.title("Stock Graph")
+window = tk.Frame(tkin, background="light pink")
+tk.Grid()
 
-plt.style.use("fivethirtyeight")
+name_abb = tk.Label(tkin, text = "Stock's Abbreviation").grid(row = 0,column = 4)
+name = tk.Entry(tkin, bg ="#C19A6B", fg="black").grid(row = 0, column = 5)
+
+start_date =  tk.Label(tkin, text = "Start Date : ").grid(row = 1,column = 4)
+start_date_in = tk.Entry(tkin, bg ="#C19A6B", fg="black").grid(row = 1, column = 5)
+
+end_date = tk.Label(tkin, text = "End Date : ").grid(row = 2,column = 4)
+end_date_in = tk.Entry(tkin, bg ="#C19A6B", fg="black").grid(row = 2, column = 5)
+
+select = tk.Label(tkin, text = "choose what you want : ").grid(row = 3,column = 3)
+
+open_t = tk.Checkbutton(tkin, onvalue = "Open", offvalue = None).grid(row = 4, column = 3)
+open_l =  tk.Label(tkin, text = "Open").grid(row = 4,column = 4)
+close_t = open_t = tk.Checkbutton(tkin, onvalue = "Close", offvalue = None).grid(row = 4, column = 5)
+close_l =  tk.Label(tkin, text = "Close").grid(row = 4,column = 6)
+high_t = tk.Checkbutton(tkin, onvalue = "High", offvalue = None).grid(row = 5, column = 3)
+high_l =  tk.Label(tkin, text = "High").grid(row = 5,column = 4)
+low_t = open_t = tk.Checkbutton(tkin, onvalue = "Low", offvalue = None).grid(row = 5, column = 5)
+low_l =  tk.Label(tkin, text = "Low").grid(row = 5,column = 6)
+
+ma_sin = tk.Checkbutton(tkin, onvalue = "MACD", offvalue = None).grid(row = 6, column = 3)
+open_l =  tk.Label(tkin, text = "MACD/Signal").grid(row = 6,column = 4)
+rsi_t = tk.Checkbutton(tkin, onvalue = "Rsi", offvalue = None).grid(row = 6, column = 5)
+rsi_l =  tk.Label(tkin, text = "RSI").grid(row = 6,column = 6)
+
+buy_t = tk.Checkbutton(tkin, onvalue = "Buy", offvalue = None).grid(row = 4, column = 3)
+buy_l =  tk.Label(tkin, text = "Buy").grid(row = 4,column = 4)
+sell_t = open_t = tk.Checkbutton(tkin, onvalue = "Sell", offvalue = None).grid(row = 4, column = 5)
+sell_l =  tk.Label(tkin, text = "Sell").grid(row = 4,column = 6)
 
 def sma(data,per = 30 ,col = "Close"):
     return data[col].rolling(window = per).mean()
@@ -62,8 +90,8 @@ class caluclate:
             else:
                 buy.append(np.nan)
                 sell.append(np.nan)
-        self.st_use["Sell"] = buy #has been change
-        self.st_use["Buy"] = sell
+        self.st_use["Sell"] = sell #has been change
+        self.st_use["Buy"] = buy
 
     def rsi(self):
         per = 14
@@ -80,7 +108,7 @@ class caluclate:
         rs = avg_gain/avg_loss
         rsi = 100.0-(100.0/(1.0+rs))
         self.st_use["rsi"] = rsi
-        
+
 
 class Plotting(caluclate):
     def __init__(self, name):
@@ -94,17 +122,23 @@ class Plotting(caluclate):
         plt.legend(loc = "upper left")
         plt.show()
 
-    def buy_sell_plot(self):
+    def buy_plot(self):
         plt.figure(figsize=(12.2,4.5))
         plt.scatter(self.st_use.index,self.st_use["Buy"],label = "Buy", color = "green", marker = "^", alpha = 1)
-        plt.scatter(self.st_use.index,self.st_use["Sell"],label = "Sell", color = "red", marker = "v", alpha = 1)
         self.st_use["Close"].plot(label = "Close Price", alpha = 0.35)
         plt.title("Close price : Buy_Sell")
         plt.xlabel("Date")
         plt.ylabel("Price")
         plt.legend(loc = "upper left")
         #plt.show()
-
+    def sell_plot(self):
+        plt.figure(figsize=(12.2,4.5))
+        plt.scatter(self.st_use.index,self.st_use["Sell"],label = "Sell", color = "red", marker = "v", alpha = 1)
+        self.st_use["Close"].plot(label = "Close Price", alpha = 0.35)
+        plt.title("Close price : Buy_Sell")
+        plt.xlabel("Date")
+        plt.ylabel("Price")
+        plt.legend(loc = "upper left")
     def rsi_plot(self):
         plt.figure(figsize=(12.2,6.4))
         plt.plot(self.st_use.index,self.st_use["rsi"],label = "rsi", color = "green")
@@ -113,11 +147,6 @@ class Plotting(caluclate):
         plt.xlabel("Date")
         plt.ylabel("Price")
         plt.legend(loc = "upper left")
-def main()
-pp = Plotting(name)
-pp.calculate()
-pp.rsi()
-pp.buy_sell()
-pp.buy_sell_plot()
-pp.rsi_plot()
-plt.show()
+
+
+tkin.mainloop()
